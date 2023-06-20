b29a8db608b05b97928784221c07eb7eaee2e3b8
Kolotylo Kyrylo <kolot102@gmail.com>
2023-06-20 13:13:14 +0300

    make todolist sorted by create_at

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -2,9 +2,14 @@ import ToDoItem from '../ToDoItem/ToDoItem';
 import styles from './ToDoList.module.css';
 
 const ToDoList = ({ data, handleDelete, openUpdateForm }) => {
+
+    const sortedData = data.sort((a, b) => {
+        return a.created_at > b.created_at ? -1 : 1;
+    });
+
     return (
         <ul className={styles.todolist}>
-            {data.map(({ id, ...rest }) => (
+            {sortedData.map(({ id, ...rest }) => (
                 <ToDoItem
                     key={id}
                     handleDelete={handleDelete}
